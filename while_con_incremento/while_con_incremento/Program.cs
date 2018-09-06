db835c490aeb584e148a2815c69ae34c3e4201fc
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-06 01:38:45 -0300

    while_con_incremento

---
+++ while_con_incremento/while_con_incremento/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace while_con_incremento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
