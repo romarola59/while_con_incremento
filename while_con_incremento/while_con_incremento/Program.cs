eac4bceb9f49a97c552fe5a36dfde7b262ab885b
Rosa Maria Rodriguez L <42683631+romarola59@users.noreply.github.com>
2018-09-06 21:27:39 -0300

    Update Program.cs

--- while_con_incremento/while_con_incremento/Program.cs
+++ while_con_incremento/while_con_incremento/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+using System;
 
-namespace while_con_incremento
+namespace while_con_incremeneto
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           int  n = 1;
+            while (n < 6)
+            {
+                Console.WriteLine(n);
+                n = n + 1;
+            }
+            Console.ReadKey();
+
         }
     }
 }
+
